5a93d0a92a19b1fbabbe05abcc9c418b87acff39
Jay Koontz <jkoontz2010@gmail.com>
2016-11-14 18:30:04 -0800

    landing page components and styling. added google maps location autocomplete

--- src/app/components/LandingPage.js
+++ src/app/components/LandingPage.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import Geosuggest from 'react-geosuggest';
+import { Well, Button } from 'React-Bootstrap';
 
 class LandingPage extends React.Component {
 	render() {
 		return (
-			<div>
-				<div>Landing Page</div>
+			<div className="container-fluid">
+				<div className="row">
+					<div className="col-sm-offset-3 col-sm-6">
+						<Well>
+							<div className="row">
+								<div className="col-sm-10">
+									<Geosuggest placeholder="What spot do you want to check out?"/>
+								</div>
+								<div className="col-sm-2">
+									<Button>Search</Button>
+								</div>
+							</div>
+							
+						</Well>
+					</div>
+				</div>
+				
 			</div>
 		)
 	}
--- src/app/components/Main.js
+++ src/app/components/Main.js
@@ -1,12 +1,26 @@
 import React from 'react';
 import { Link } from 'react-router';
+import { Navbar, Nav, NavItem } from 'React-Bootstrap';
 
 class Main extends React.Component {
 	render() {
 		return (
 			<div>
-				<h1>
-					<Link to="/">Spots</Link>
+				<Navbar collapseOnSelect>
+				    <Navbar.Header>
+				      <Navbar.Brand>
+				        <Link to="/">Spots</Link>
+				      </Navbar.Brand>
+				      <Navbar.Toggle />
+				    </Navbar.Header>
+				    <Navbar.Collapse>
+				      <Nav pullRight>
+				        <NavItem eventKey={1} href="#">Log In</NavItem>
+				      </Nav>
+				    </Navbar.Collapse>
+				  </Navbar>
+				<h1 className="text-center header">
+					The one-stop shop for all your pictures of a spot
 				</h1>
 				{React.cloneElement(this.props.children, this.props)}
 			</div>
